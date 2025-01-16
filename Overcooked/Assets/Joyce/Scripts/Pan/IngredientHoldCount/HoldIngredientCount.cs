141705610a2f81a48f42d4093de444baf2bdf13e
JoyceMolenaar0 <146432917+JoyceMolenaar0@users.noreply.github.com>
2025-01-16 09:39:40 +0100

    canCook
    je krijgt nu icoontjes met het koken, zodat je weet hoeveel ingredienten er in zitten en hoe veel je er nog in kan doen

--- Overcooked/Assets/Joyce/Scripts/Pan/IngredientHoldCount/HoldIngredientCount.cs
+++ Overcooked/Assets/Joyce/Scripts/Pan/IngredientHoldCount/HoldIngredientCount.cs
@@ -2,6 +2,35 @@ using UnityEngine;
 
 public class HoldIngredientCount : MonoBehaviour
 {
+    [SerializeField] GameObject noImages;
+    [SerializeField] GameObject allImages;
+    [SerializeField] GameObject[] gotImages = new GameObject[3];
     public int ingredientCount { get; set; }
+
+    private void Update()
+    {
+        for (int i = 0; i < gotImages.Length; i++)
+        {
+            if (i < ingredientCount)
+            {
+                gotImages[i].SetActive(true);
+            }
+            else
+            {
+                gotImages[i].SetActive(false );
+            }
+        }
+
+        if (ingredientCount > 0)
+        {
+            noImages.SetActive(false);
+            allImages.SetActive(true);
+        }
+        else
+        {
+            noImages.SetActive(true);
+            allImages.SetActive(false);
+        }
+    }
 }
 
